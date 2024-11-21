7c67c6ab9584057e716c1689d6c03131bea42535
LingGrani <luthfiangger2510@gmail.com>
2024-11-21 21:28:17 +0700

    update -- "Add Header and add uiState"

--- app/src/main/java/com/example/p8casestudy/ui/view/screen/TampilView.kt
+++ app/src/main/java/com/example/p8casestudy/ui/view/screen/TampilView.kt
@@ -1,31 +1,43 @@
 package com.example.p8casestudy.ui.view.screen
 
 import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.p8casestudy.R
 import com.example.p8casestudy.ui.model.Mahasiswa
+import com.example.p8casestudy.ui.model.RencanaStudi
 
-@Preview(showBackground = true)
 @Composable
 fun TampilView(
-    uiState: Mahasiswa,
+    uiStateMhs: Mahasiswa,
+    uiStateMK: RencanaStudi,
     onBackButtonClicked: () -> Unit = {},
     onResetButtonClicked: () -> Unit = {}
 ) {
@@ -34,6 +46,42 @@ fun TampilView(
             .fillMaxSize().background(colorResource(id = R.color.primary)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(id =  R.drawable.img),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1f)){
+                Text(
+                    text = uiStateMhs.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = uiStateMhs.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Box {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+        }
         Column (
             modifier = Modifier.fillMaxSize()
                 .background(Color.White)
@@ -47,13 +95,13 @@ fun TampilView(
                 Column (
                 ){
                     Text("Nim:")
-                    Text(uiState.nim)
+                    Text(uiStateMhs.nim)
                     Text("Nama:")
-                    Text(uiState.nama)
+                    Text(uiStateMhs.nama)
                 }
                 Column (
                 ){
-                    Text(uiState.email)
+                    Text(uiStateMhs.email)
                 }
             }
             Spacer(modifier = Modifier.padding(16.dp))
@@ -61,10 +109,10 @@ fun TampilView(
                 modifier = Modifier.fillMaxWidth()
             ){
                 Text("Matakuliah yang diabli:")
-                Text(uiState.namaMatakuliah)
+                Text(uiStateMK.namaMK)
                 Row(){
                     Text(text = "Kelas:")
-                    Text(uiState.kelas)
+                    Text(uiStateMK.kelas)
                 }
             }
             Spacer(modifier = Modifier.padding(32.dp))
